7331d47a397a33382d92f8e0ff98a12a169a77ef
Eunji Kwon <kwonge08e@gmail.com>
2024-02-20 18:24:21 -0800

    Update

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDVzDtE-nqEMOYsWK4XG7wwn83OjJlGeuQ",
+    authDomain: "comp1800-202410-demo-4b86b.firebaseapp.com",
+    projectId: "comp1800-202410-demo-4b86b",
+    storageBucket: "comp1800-202410-demo-4b86b.appspot.com",
+    messagingSenderId: "652423483042",
+    appId: "1:652423483042:web:2b4281503058b5fad751fb"
 };
 
 //--------------------------------------------
